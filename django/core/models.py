7e0b48b8b766b8203eaa77edd711d690d216db86
Alexandre Amat <alex.amat@i-drc.com>
2020-08-16 16:07:26 +0800

    initial commit, trying django models

---
+++ django/core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    
+    username = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.name} {self.surname}'
