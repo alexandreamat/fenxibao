33992dd2bfaf2f3abc349fdacfdff4769793caa2
Alexandre Amat <alex.amat@i-drc.com>
2020-10-07 14:06:42 +0800

    added constant classes with options in csv file

--- app/core/management/commands/provision_alipay_records.py
+++ app/core/management/commands/provision_alipay_records.py
@@ -19,7 +19,13 @@ ENCODING = 'gb18030'
 PATTERN = r':\[(.*?)\]'
 ACCOUNT_ZH = '账号'
 
-class TransactionRecord:
+class AlipayRecord:
+
+    class FileSection(Enum):
+        HEADER = auto()
+        LABELS = auto()
+        BODY = auto()
+        FOOTER = auto()
 
     class Label(Enum):
         '''Labels of record CSV file
@@ -32,25 +38,62 @@ class TransactionRecord:
         ORDER_NUM = 1 #: 商家订单号
         CREATION_DATE = 2 #: 交易创建时间
         LAST_MODIFIED_DATE = 3 #: 最近修改时间
-        PAYMENT_TIME = 4 #: 付款时间
+        PAYMENT_DATE = 4 #: 付款时间
         ORIGIN = 5 #: 交易来源地
         TYPE = 6 #: 类型
         COUNTERPART = 7 #: 交易对方
         PRODUCT_NAME = 8 #: 商品名称
         AMOUNT = 9 #: 金额（元）
         SIGN = 10 #: 收/支
-        STATUS = 11 #: 交易状态
+        STATE = 11 #: 交易状态
         SERVICE_FEE = 12 #: 服务费（元）
         REFUND_COMPLETE = 13 #: 成功退款（元）
         NOTES = 14 #: 备注
         FUNDS_STATE = 15 #: 资金状态
 
 
-    class FileSection(Enum):
-        HEADER = auto()
-        LABELS = auto()
-        BODY = auto()
-        FOOTER = auto()
+    class TransactionOrigin:
+        TAOBAO = '淘宝'
+        ALIPAY = '支付宝网站'
+        OTHER = '其他（包括阿里巴巴和外部商家）'
+    
+
+    class TransactionType:
+        ALIPAY_PROTECTED = '支付宝担保交易'
+        BOOKING = '预订交易'
+        INSTANT = '即时到账交易'
+
+
+    class Sign:
+        EXPENDITURE = '支出'
+        INCOME = '收入'
+        EMPTY = ''
+
+
+    class State:
+        AWAITING_RECEPTION_CONFIRMATION = '等待确认收货'
+        CLOSED = '已关闭'
+        PAID = '支付成功'
+        TRANSFER_FAILED = '转账失败'
+        ADDED_VALUE = '充值成功'
+        FAILURE = '失败'
+        UNFREEZED = '解冻成功'
+        PAID_BY_OTHERS = '代付成功'
+        FREEZED = '冻结成功'
+        AWAITING_PAYMENT = '等待付款'
+        REFUNDED = '退款成功'
+        TRANSACTION_SUCCESSFUL = '交易成功'
+        TRANSACTION_CLOSED = '交易关闭'
+
+
+    class FundState:
+        AWAITING_EXPENDITURE = '待支出'
+        PAID = '已支出'
+        FUNDS_TRANSFER = '资金转移'
+        FROZEN = '冻结'
+        RECEIVED = '已收入'
+        UNFROZEN = '解冻'}
+        EMPTY = ''
 
 
     def __init__(self, file_paths: str):
@@ -76,6 +119,11 @@ class TransactionRecord:
         alipay_id = columns[self.Label.NUM.value]
         creation_date = columns[self.Label.CREATION_DATE.value]
         amount = columns[self.Label.AMOUNT.value]
+        origin = columns[self.Label.ORIGIN.value]
+        type_ = columns[self.Label.TYPE.value]
+        sign = columns[self.Label.SIGN.value]
+        state = columns[self.Label.STATE.value]
+        funds_state = columns[self.Label.FUNDS_STATE.value]
         account, _ = Account.objects.get_or_create(username=self.username)
         Transaction.objects.get_or_create(
             alipay_id=alipay_id,
@@ -134,4 +182,4 @@ class TransactionRecord:
 def command(file_paths):
     click.secho("Processing records files...")
     with transaction.atomic():
-        TransactionRecord(file_paths=glob.glob(file_paths))
+        AlipayRecord(file_paths=glob.glob(file_paths))
